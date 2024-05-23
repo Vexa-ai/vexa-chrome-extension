6cf8cf32a7d09236c4120a5c2f4b7a8fb3d6bfcc
Ikenna Okafor <robotik.anthony@gmail.com>
2024-05-23 14:53:03 +0100

    Disable recording if there's no meeting ID

--- src/shared/components/VexaPlayButton/VexaPlayButton.tsx
+++ src/shared/components/VexaPlayButton/VexaPlayButton.tsx
@@ -5,6 +5,7 @@ import './VexaPlayButton.scss';
 import { useAudioCapture } from '~shared/hooks/use-audiocapture';
 import { useStorage } from '@plasmohq/storage/hook';
 import { StorageService, StoreKeys } from '~lib/services/storage.service';
+import { getIdFromUrl } from '~shared/helpers/meeting.helper';
 
 export interface VexaPlayButtonProps {
   [key: string]: any;
@@ -13,6 +14,7 @@ export interface VexaPlayButtonProps {
 export function VexaPlayButton({ ...rest }: VexaPlayButtonProps) {
   const audioCapture = useAudioCapture();
   const [selectedMicrophone] = StorageService.useHookStorage(StoreKeys.SELECTED_MICROPHONE);
+  const meetingId = getIdFromUrl(location.href);
   const startCapture = () => {
     audioCapture.startAudioCapture();
   }
@@ -21,7 +23,7 @@ export function VexaPlayButton({ ...rest }: VexaPlayButtonProps) {
   }, [selectedMicrophone]);
 
   return <div {...rest} className='VexaPlayButton'>
-    <button disabled={!selectedMicrophone} onClick={startCapture} className='bg-[#9E77ED] hover:bg-[#b492f8] disabled:bg-[#CECFD2] p-2 flex gap-1 items-center justify-center rounded-lg font-medium text-white'>
+    <button disabled={!selectedMicrophone || !meetingId} onClick={startCapture} className='bg-[#9E77ED] hover:bg-[#b492f8] disabled:bg-[#CECFD2] p-2 flex gap-1 items-center justify-center rounded-lg font-medium text-white'>
       <img alt='' className='w-5 h-5' src={playIcon} />
       <span>Start recording</span>
     </button>

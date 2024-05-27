719c09ee8c75de5d14e162956273a0da9a69d83e
Ikenna Okafor <robotik.anthony@gmail.com>
2024-05-27 20:18:14 +0100

    Reset draggable UIs positions if dropped outside the screen

--- src/contents/vexa-btn.tsx
+++ src/contents/vexa-btn.tsx
@@ -1,7 +1,7 @@
 import type { PlasmoCSConfig } from "plasmo";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-import Draggable from "react-draggable";
+import Draggable, { type DraggableData, type DraggableEvent } from "react-draggable";
 import rootCssText from "data-text:~root.scss";
 import vexaBtnCss from 'data-text:./vexa-btn.scss';
 import { StorageService, StoreKeys } from "~lib/services/storage.service";
@@ -12,11 +12,51 @@ import { getIdFromUrl } from "~shared/helpers/meeting.helper";
 const VexaBtn = () => {
     const [isMaximized, setIsMaximized] = StorageService.useHookStorage<boolean>(StoreKeys.WINDOW_STATE, true);
     const isValidContext = getIdFromUrl(location.href);
+    const defaultPosition = { x: 0, y: 0 };
+    const [position, setPosition] = useState(defaultPosition);
+    
+    const handleDrag = (e: DraggableEvent, data: DraggableData) => {
+        setPosition({ x: data.x, y: data.y });
+    };
+
+    const handleStop = (e: DraggableEvent, data: DraggableData) => {
+        const { clientWidth, clientHeight } = document.documentElement;
+        const { node } = data;
+        const rect = node.getBoundingClientRect();
+        console.log(rect, {rect, clientWidth, clientHeight });
+        if (rect.right < 0 || (rect.top > clientHeight || rect.bottom < 0) || rect.left > clientWidth) {
+            setPosition(defaultPosition);
+        }
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (isMaximized) {
+                const { clientWidth, clientHeight } = document.documentElement;
+                const node = document.getElementById("vexa-content-div");
+                if (node) {
+                    const rect = node.getBoundingClientRect();
+                    if (rect.right < 0 || (rect.top > clientHeight || rect.bottom < 0) || rect.left > clientWidth) {
+                        setPosition(defaultPosition);
+                    }
+                }
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [isMaximized]);
 
     return (
         <>
             {
-                isValidContext && !isMaximized && (<Draggable>
+                isValidContext && !isMaximized && (<Draggable
+                    position={position}
+                    onDrag={handleDrag}
+                    onStop={handleStop}
+                >
                     <button style={{
                         top: 'calc(50vh - 29px) !important',
                         right: '20px !important',
--- src/contents/vexa.tsx
+++ src/contents/vexa.tsx
@@ -1,10 +1,10 @@
 import type { PlasmoCSConfig, PlasmoGetInlineAnchor, PlasmoGetOverlayAnchor, PlasmoRender } from "plasmo";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { MainContentView, MicrophoneOptions, VexaToolbar } from "~shared/components";
 import { AudioCaptureContext, useAudioCapture } from "~shared/hooks/use-audiocapture";
 import { MessageSenderService } from "~lib/services/message-sender.service";
 import { StorageService, StoreKeys } from "~lib/services/storage.service";
-import Draggable from "react-draggable";
+import Draggable, { type DraggableData, type DraggableEvent } from "react-draggable";
 
 const messageSender = new MessageSenderService();
 
@@ -12,11 +12,51 @@ const Vexa = () => {
     const audioCapture = useAudioCapture();
     const [isCapturing] = StorageService.useHookStorage<boolean>(StoreKeys.CAPTURING_STATE);
     const [isMaximized] = StorageService.useHookStorage<boolean>(StoreKeys.WINDOW_STATE, true);
+    const defaultPosition = { x: 0, y: 0 };
+    const [position, setPosition] = useState(defaultPosition);
+
+    const handleDrag = (e: DraggableEvent, data: DraggableData) => {
+        setPosition({ x: data.x, y: data.y });
+    };
+
+    const handleStop = (e: DraggableEvent, data: DraggableData) => {
+        const { clientWidth, clientHeight } = document.documentElement;
+        const { node } = data;
+        const rect = node.getBoundingClientRect();
+        console.log(rect, {rect, clientWidth, clientHeight });
+        if (rect.right < 0 || (rect.top > clientHeight || rect.bottom < 0) || rect.left > clientWidth) {
+            setPosition(defaultPosition);
+        }
+    };
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (isMaximized) {
+                const { clientWidth, clientHeight } = document.documentElement;
+                const node = document.getElementById("vexa-content-div");
+                if (node) {
+                    const rect = node.getBoundingClientRect();
+                    if (rect.right < 0 || (rect.top > clientHeight || rect.bottom < 0) || rect.left > clientWidth) {
+                        setPosition(defaultPosition);
+                    }
+                }
+            }
+        };
+
+        window.addEventListener("resize", handleResize);
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        };
+    }, [isMaximized]);
 
     return (
         <>
             {
-                isMaximized && (<Draggable>
+                isMaximized && (<Draggable
+                    position={position}
+                    onDrag={handleDrag}
+                    onStop={handleStop}
+                >
                     <div id="vexa-content-div" className="flex flex-col h-screen w-[400px] bg-slate-950 px-4 pt-4 pb-4 overflow-y-auto overflow-x-hidden">
                         <AudioCaptureContext.Provider value={audioCapture}>
                             <VexaToolbar />
